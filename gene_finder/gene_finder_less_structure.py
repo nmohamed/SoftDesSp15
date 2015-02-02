14fa24778e9b6a081ee6b24f5c4cb8ed16e4cb25
Nora Mohamed <jurukix@gmail.com>
2015-02-01 20:16:43 -0500

    fixed find_ORF not working if dna doesn't end with end codon

--- gene_finder/gene_finder_less_structure.py
+++ gene_finder/gene_finder_less_structure.py
@@ -12,13 +12,12 @@ from load import load_seq
 #TO DO: Add function that checks to make sure DNA string is actually DNA
 
 def find_ORF(dna):
-    """ Takes a DNA sequence that is assumed to begin with a start codon and returns
-        the sequence up to but not including the first in frame stop codon.  If there
-        is no in frame stop codon, returns the whole string.
+    """ Takes a DNA sequence and returns every ORF, not including stop codons.
+        If there is no in frame stop codon, returns the whole string.
         
         dna: a DNA sequence
-        returns: the open reading frame represented as a string
-    >>> find_ORF("ATGAGATAGGATGAAAAAATAA")
+        returns: all open reading frames represented as a string in array
+    >>> find_ORF("CATGAGATAGGATGAAAAAATAA")
     ['ATGAGA', 'ATGAAAAAA']
     """
     start = "ATG"
@@ -27,29 +26,37 @@ def find_ORF(dna):
         #Finds start codon
         if start in dna: #checks to make sure start codon can be found
             start_index = dna.index(start) #find index of ATG
-            new_ORF = dna[start_index:len(dna)] #new ORF, placeholder type
+            new_ORF = dna[start_index:len(dna)] #new ORF
+
+            if new_ORF == "ATG":
+                r_ORF.append(new_ORF[0:x]) #if whats left is just "ATG"
+                return r_ORF
+
             #check every three indices after for TAG TGA TAA
             for x in range(3, len(new_ORF), 3): 
                 if new_ORF[x:x+3] == "TAG" or new_ORF[x:x+3] == "TGA" or new_ORF[x:x+3] == "TAA":
                     r_ORF.append(new_ORF[0:x]) #add to array new ORF
-                    dna = new_ORF[x+3:len(new_ORF)] #replace dna with 
+                    dna = new_ORF[x+3:len(new_ORF)] #replace dna with
                     break
+                elif x + 3 >= len(new_ORF):
+                    #if theres no end codon and you reach the end of the sequence
+                    #add the sequence without the end codon 
+                    r_ORF.append(new_ORF)
+                    return r_ORF
         else: #if start can't be found, finish
             return r_ORF
-    #note: could change new_ORF to dna? look it over again
 
 def find_reverse_complement(dna):
-    """ Finds complementary strand to input (doesn't reverse it)
+    """ Finds complementary strand to input and reverses
 
         dna: a DNA sequence
-        returns: the complementary strand of dna
-    >>> find_complement("ATCG")
+        returns: the reverse complementary strand of dna
+    >>> find_reverse_complement("ATCG")
     'CGAT'
     """
     dna = dna[::-1] #reverse string
     new = ""
     for x in range(0, len(dna)): #check for compliment
-        print dna[x]
         if dna[x] == 'A':
             new = new + 'T'
         elif dna[x] == 'T':
@@ -76,7 +83,7 @@ def find_all_ORFs_both_strands(dna):
     first_strand = find_ORF(dna)
     #find ORF for second strand (find reverse complement of dna)
     second_strand = find_ORF(find_reverse_complement(dna))
-    return first_strand.append(second_strand)
+    return first_strand + second_strand
 
 def longest_ORF(dna):
     """ Finds the longest ORF on both strands of the specified DNA and returns it
