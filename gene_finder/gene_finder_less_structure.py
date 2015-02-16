112e6eac1bf6caaf75e222f6e338cf09e191d5fe
Nora Mohamed <jurukix@gmail.com>
2015-02-16 00:28:18 -0500

    formatting changes

--- gene_finder/gene_finder_less_structure.py
+++ gene_finder/gene_finder_less_structure.py
@@ -22,28 +22,24 @@ def find_ORF(dna):
     """
     start = "ATG"
     r_ORF = []
-    while True: #looping to find every ORF
-        #Finds start codon
-        if start in dna: #checks to make sure start codon can be found
-            start_index = dna.index(start) #find index of ATG
-            new_ORF = dna[start_index:len(dna)] #new ORF
+    while True: 
+        if start in dna: 
+            start_index = dna.index(start) 
+            new_ORF = dna[start_index:len(dna)] 
 
             if new_ORF == "ATG":
-                r_ORF.append(new_ORF) #if whats left is just "ATG"
+                r_ORF.append(new_ORF)
                 return r_ORF
 
-            #check every three indices after for TAG TGA TAA
             for x in range(3, len(new_ORF), 3): 
                 if new_ORF[x:x+3] == "TAG" or new_ORF[x:x+3] == "TGA" or new_ORF[x:x+3] == "TAA":
-                    r_ORF.append(new_ORF[0:x]) #add to array new ORF
-                    dna = new_ORF[x+3:len(new_ORF)] #replace dna with
+                    r_ORF.append(new_ORF[0:x])
+                    dna = new_ORF[x+3:len(new_ORF)]
                     break
                 elif x + 3 >= len(new_ORF):
-                    #if theres no end codon and you reach the end of the sequence
-                    #add the sequence without the end codon 
                     r_ORF.append(new_ORF)
                     return r_ORF
-        else: #if start can't be found, finish
+        else: 
             return r_ORF
 
 def find_reverse_complement(dna):
@@ -54,9 +50,9 @@ def find_reverse_complement(dna):
     >>> find_reverse_complement("ATCG")
     'CGAT'
     """
-    dna = dna[::-1] #reverse string
+    dna = dna[::-1] 
     new = ""
-    for x in range(0, len(dna)): #check for compliment
+    for x in range(0, len(dna)):
         if dna[x] == 'A':
             new = new + 'T'
         elif dna[x] == 'T':
@@ -78,9 +74,7 @@ def find_all_ORFs_both_strands(dna):
     >>> find_all_ORFs_both_strands("ATGCGAATGTAGCATCAAA")
     ['ATGCGAATG', 'ATGCTACATTCGCAT']
     """
-    #find ORF for first strand
     first_strand = find_ORF(dna)
-    #find ORF for second strand (find reverse complement of dna)
     second_strand = find_ORF(find_reverse_complement(dna))
     return first_strand + second_strand
 
@@ -94,9 +88,9 @@ def longest_ORF(dna):
     if len(ORFs) > 1:
         longest = max(ORFs, key=len)
         return longest
-    elif len(ORFs) == 0: #if there were no ORFs
+    elif len(ORFs) == 0: 
         return "x"
-    elif len(ORFs) == 1: #if there was only one ORF
+    elif len(ORFs) == 1: 
         return ORFs[0]
 
     #what should this do if they're the same length? and will there only be
@@ -133,7 +127,6 @@ def coding_strand_to_AA(dna):
     >>> coding_strand_to_AA("ATGCCCGCTTT")
     'MPA'
     """
-    #dna = longest_ORF_noncoding(dna, 20)
     polymer = ""
     for x in range(0, len(dna), 3):
         if x + 3 > len(dna):
@@ -143,7 +136,6 @@ def coding_strand_to_AA(dna):
             polymer = polymer + amino_acid
     return polymer
 
-<<<<<<< HEAD
 
 def gene_finder(dna):
     """ Returns the amino acid sequences coded by all genes that have an ORF
@@ -154,35 +146,27 @@ def gene_finder(dna):
     returns: a list of all amino acid sequences whose ORFs meet the minimum
     length specified.
     """
-    threshold = len(longest_ORF_noncoding(dna, 1500))  #divided by three for each codon
-    print "Threshold =", threshold
+    threshold = len(longest_ORF_noncoding(dna, 1500))  
     all_ORFs = find_all_ORFs_both_strands(dna)
-    #print all_ORFs
 
     aas = []
     pass_ORFs = []
-    for x in range(0, len(all_ORFs)): #translates to aa
+    for x in range(0, len(all_ORFs)): 
         if len(all_ORFs[x]) >= threshold:
             pass_ORFs.append(all_ORFs[x])
 
     for x in range(0, len(pass_ORFs)):
         aas.append(coding_strand_to_AA(pass_ORFs[x]))
 
-=======
 def gene_finder(dna):
     """ Returns the amino acid sequences that are likely coded by the specified dna
         
         dna: a DNA sequence
         returns: a list of all amino acid sequences coded by the sequence dna.
->>>>>>> 922a6e32441860ab0413630f74531e6e47a16a7c
     """
-    for x in range(0, len(aas)): #sees if aa sequence passes threshold
+    for x in range(0, len(aas)):
         if len(aas[x]) >= threshold:
             pass_aas.append(aas[x])
-            """
-    #print aas
-    print len(aas)
-    print aas
     return aas
 
 
