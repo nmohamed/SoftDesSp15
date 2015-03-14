9fdccfdb482ee88f2a9000e6482591da559b598c
Nora Mohamed <jurukix@gmail.com>
2015-03-13 22:03:49 -0400

    did toolboxes

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -9,7 +9,23 @@ def get_word_list(file_name):
 		returns a list of the words used in the book as a list.
 		All words are converted to lower case.
 	"""
-	pass
+	# First create list of lines
+	lines = []
+	f = open(file_name,'r')
+	lines = f.readlines()
+	curr_line = 0
+	while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+		curr_line += 1
+	lines = lines[curr_line+1:]
+	#print lines
+	# Next make list of everyword
+	words = []
+	for line in lines:
+		stripped = line.split()
+		for word in stripped:
+			words.append(word.lower().strip('\n').strip(string.punctuation))
+	return words
+
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -19,6 +35,14 @@ def get_top_n_words(word_list, n):
 					punctuation
 		n: the number of words to return
 		returns: a list of n most frequently occurring words ordered from most
-				 frequently to least frequentlyoccurring
+				 frequently to least frequently occurring
 	"""
-	pass
+	dictionary = {}
+	for words in word_list:
+		dictionary[words] = dictionary.get(words, 0) + 1
+
+	sort = sorted(dictionary, key = dictionary.get, reverse = True)
+	top_words = sort[:n]
+	print top_words
+	
+get_top_n_words(get_word_list('pg32325.txt'), 100)
