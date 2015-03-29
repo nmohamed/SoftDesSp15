504be957911e1c260393eab94da8a415fa7eeae1
Nora Mohamed <jurukix@gmail.com>
2015-03-29 15:12:00 -0400

    Finished AI paths, machine learning, and opencd toolboxes

--- toolbox/image_processing/face_detect.py
+++ toolbox/image_processing/face_detect.py
@@ -2,3 +2,35 @@
 
 import cv2
 import numpy as np
+
+kernel = np.ones((21,21),'uint8')
+face_cascade = cv2.CascadeClassifier('/home/nora/SoftDesSp15/toolbox/image_processing/haarcascade_frontalface_alt.xml')
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+
+	ret, frame = cap.read()
+	faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minSize=(20,20))
+	for (x,y,w,h) in faces:
+		frame[y:y+h,x:x+w,:] = cv2.dilate(frame[y:y+h,x:x+w,:], kernel)
+		# cv2.rectangle(frame,(x,y),(x+w,y+h),(0,(int(x+.4*w), int(y+.8*h))0,255))
+		cv2.circle(frame, (int(x+.3*w), int(y+.3*h)), 20, (255, 255, 255), 15)
+		cv2.circle(frame, (int(x+.3*w), int(y+.3*h)), 3, (0, 0, 0), 15 )
+		cv2.circle(frame, (int(x+.7*w), int(y+.3*h)), 20, (255, 255, 255), 15 )
+		cv2.circle(frame, (int(x+.7*w), int(y+.3*h)), 3, (0, 0, 0), 15 )
+		# cv2.ellipse(frame, (int(x+.5*w), int(y+.7*h)), (0, 0, 255), 15)
+		cv2.circle(frame, (int(x+.5*w), int(y+.8*h)), 3, (0, 0, 255), 40 )
+		cv2.line(frame, (int(x+.4*w), int(y+.8*h)), 
+						(int(x+.6*w), int(y+.8*h)),
+						(0,0,0), 5)
+
+	# Display the resulting frame
+	cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -7,17 +7,31 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 data = load_digits()
-print data.DESCR
+#print data.DESCR
 num_trials = 10
 train_percentages = range(5,95,5)
 test_accuracies = numpy.zeros(len(train_percentages))
+
+count = 0
+
+for train_sizes in train_percentages:
+	trial_acc = []
+	for i in range(0, num_trials): # do 10 trials for each percentage
+		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=train_sizes)
+		model = LogisticRegression(C=10**-20)
+		model.fit(X_train, y_train)
+		#print "Train accuracy %f" %model.score(X_train,y_train)
+		#print "Test accuracy %f" %model.score(X_test,y_test)
+		trial_acc.append(model.score(X_test,y_test))
+	test_accuracies[count] = sum(trial_acc)/len(trial_acc)
+	count+=1
+
 
 # train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
 # the resultant accuracy.
 # You should repeat each training percentage num_trials times to smooth out variability
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
-# TODO: your code here
 
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
